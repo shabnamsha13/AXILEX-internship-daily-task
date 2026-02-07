22391385695b83f5b98172ed7dac27b85f6253e6
shabnamsha13 <148140705+shabnamsha13@users.noreply.github.com>
2026-02-07 17:21:56 +0530

    Add files via upload

---
+++ Day-4/loops/for_loop.py
@@ -0,0 +1,9 @@
+#Print numbers from 1 to 5 using for loop
+for i in range(1, 6):
+    print(i)
+#Sum of numbers from 1 to 5 using for loop
+sum = 0
+for i in range(1, 6):
+    sum += i
+print("Sum:", sum)
+
