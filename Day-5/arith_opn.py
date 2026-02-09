befcd9f0c09b918b4c8ed23444521f98f5c23aa5
shabnamsha13 <148140705+shabnamsha13@users.noreply.github.com>
2026-02-09 20:45:21 +0530

    Add files via upload

---
+++ Day-5/arith_opn.py
@@ -0,0 +1,20 @@
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
+print("Multiplication:", multiply(a, b))
+print("Division:", divide(a, b))
