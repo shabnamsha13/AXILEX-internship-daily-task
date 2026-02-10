c9887e5e2721692fc6fe89dd821924e41c15619a
shabnamsha13 <148140705+shabnamsha13@users.noreply.github.com>
2026-02-10 11:43:27 +0530

    Add files via upload

---
+++ Week-2/Day-1/ascending_descending/sort_order.py
@@ -0,0 +1,9 @@
+#Using sorted() function
+numbers = [13, 26, 1, 76, 30, 5]
+
+ascending = sorted(numbers)
+descending = sorted(numbers, reverse=True)
+
+print("Original list:", numbers)
+print("Ascending order:", ascending)
+print("Descending order:", descending)
